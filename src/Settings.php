a6ed26e266a3e71249c8a639c377a3d921dce48d
Joe Huss <detain@interserver.net>
2017-05-24 19:12:31 -0400

    Updates use the get_Setting call instead of directly refering ot a defined value

--- src/Settings.php
+++ src/Settings.php
@@ -12,8 +12,8 @@ class Settings {
 	public static function update(GenericEvent $event) {
 		// will be executed when the licenses.settings event is dispatched
 		$settings = $event->getSubject();
-		$settings->add_text_setting('apisettings', 'fantastico_username', 'Fantastico Username:', 'Fantastico Username', FANTASTICO_USERNAME);
-		$settings->add_text_setting('apisettings', 'fantastico_password', 'Fantastico Password:', 'Fantastico Password', FANTASTICO_PASSWORD);
+		$settings->add_text_setting('apisettings', 'fantastico_username', 'Fantastico Username:', 'Fantastico Username', $settings->get_setting('FANTASTICO_USERNAME'));
+		$settings->add_text_setting('apisettings', 'fantastico_password', 'Fantastico Password:', 'Fantastico Password', $settings->get_setting('FANTASTICO_PASSWORD'));
 	}
 
 }
