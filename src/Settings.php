68211a23e96aa025600b2ac05be75ed8bb1d1965
Joe Huss <detain@interserver.net>
2017-05-24 18:57:54 -0400

    updates adding a settings class

---
+++ src/Settings.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace detain\MyAdmin-Fantastico;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class Settings {
+
+	public function __construct() {
+	}
+
+	public static function update(GenericEvent $event) {
+		// will be executed when the licenses.settings event is dispatched
+		$settings = $event->getSubject();
+		$settings->add_text_setting('apisettings', 'fantastico_username', 'Fantastico Username:', 'Fantastico Username', FANTASTICO_USERNAME);
+		$settings->add_text_setting('apisettings', 'fantastico_password', 'Fantastico Password:', 'Fantastico Password', FANTASTICO_PASSWORD);
+	}
+
+}
