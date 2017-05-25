39b05e62b80a83d4df61c6d5da99f8163275caa4
Joe Huss <detain@interserver.net>
2017-05-25 19:14:18 -0400

    updates

--- myadmin.php
+++ myadmin.php
@@ -10,8 +10,8 @@ return [
 	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a fantastico license. Allow 10 minutes for activation.',
 	'module' => 'licenses',
 	'author' => 'detain@interserver.net',
-	'home' => 'https://github.com/detain/myadmin-fantastico',
-	'repo' => 'https://github.com/detain/myadmin-fantastico',
+	'home' => 'https://github.com/detain/myadmin-fantastico-licensing',
+	'repo' => 'https://github.com/detain/myadmin-fantastico-licensing',
 	'version' => '1.0.0',
 	'type' => 'licenses',
 	'hooks' => [
