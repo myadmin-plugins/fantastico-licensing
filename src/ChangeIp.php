8318567198c53bf47572fc477d9934506de0661f
Joe Huss <detain@interserver.net>
2017-05-25 10:50:50 -0400

    Updates adding the code for the change ip event

--- src/ChangeIp.php
+++ src/ChangeIp.php
@@ -2,6 +2,7 @@
 
 namespace Detain\MyAdminFantastico;
 
+use Detain\Fantastico\Fantastico;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 class ChangeIp {
@@ -10,12 +11,22 @@ class ChangeIp {
 	}
 
 	public static function update(GenericEvent $event) {
-		// will be executed when the licenses.license event is dispatched
-		$license = $event->getSubject();
 		if ($event['category'] == SERVICE_TYPES_FANTASTICO) {
-			myadmin_log('licenses', 'info', 'Fantastico Activation', __LINE__, __FILE__);
-			function_requirements('activate_fantastico');
-			activate_fantastico($license->get_ip(), $event['field1']);
+			$license = $event->getSubject();
+			$settings = get_module_settings('licenses');
+			$fantastico = new Fantastico(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
+			myadmin_log('licenses', 'info', "IP Change - (OLD:".$license->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
+			$result = $fantastico->editIp($license->get_ip(), $event['newip']);
+			if (isset($result['faultcode'])) {
+				myadmin_log('licenses', 'error', 'Fantastico editIp('.$license->get_ip().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
+				$event['status'] = 'error';
+				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
+			} else {
+				$GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $license->get_ip());
+				$license->set_ip($event['newip'])->save();
+				$event['status'] = 'ok';
+				$event['status_text'] = 'The IP Address has been changed.';
+			}
 			$event->stopPropagation();
 		}
 	}
