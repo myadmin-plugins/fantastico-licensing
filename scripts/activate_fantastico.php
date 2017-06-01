6b70bc0659ee525f78b6e37fe3966a9a117bb52b
Joe Huss <detain@interserver.net>
2017-05-31 23:22:58 -0400

    adding scripts

---
+++ scripts/activate_fantastico.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+
+require_once(__DIR__ . '/../../../include/functions.inc.php');
+$GLOBALS['tf']->session->create(160308, 'services');
+$GLOBALS['tf']->session->verify();
+
+activate_fantastico('66.23.229.238', 2);
+
+$GLOBALS['tf']->session->destroy();
