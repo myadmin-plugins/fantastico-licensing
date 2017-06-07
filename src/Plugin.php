d4412b11d2d7df6d84d107957f9dfd9dfa309faf
Joe Huss <detain@interserver.net>
2017-06-07 07:39:52 -0400

    updates to settings

--- src/Plugin.php
+++ src/Plugin.php
@@ -73,8 +73,8 @@ class Plugin {
 	public static function Settings(GenericEvent $event) {
 		// will be executed when the licenses.settings event is dispatched
 		$settings = $event->getSubject();
-		$settings->add_text_setting('apisettings', 'fantastico_username', 'Fantastico Username:', 'Fantastico Username', $settings->get_setting('FANTASTICO_USERNAME'));
-		$settings->add_text_setting('apisettings', 'fantastico_password', 'Fantastico Password:', 'Fantastico Password', $settings->get_setting('FANTASTICO_PASSWORD'));
+		$settings->add_text_setting('licenses', 'apisettings', 'fantastico_username', 'Fantastico Username:', 'Fantastico Username', $settings->get_setting('FANTASTICO_USERNAME'));
+		$settings->add_text_setting('licenses', 'apisettings', 'fantastico_password', 'Fantastico Password:', 'Fantastico Password', $settings->get_setting('FANTASTICO_PASSWORD'));
 		$settings->add_dropdown_setting('stock', 'outofstock_licenses_fantastico', 'Out Of Stock Fantastico Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_FANTASTICO'), array('0', '1'), array('No', 'Yes', ));
 	}
 
