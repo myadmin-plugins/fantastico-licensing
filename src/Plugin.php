1b83353e24ba1a8b0bb57b57cd5c399ee10be101
Joe Huss <detain@interserver.net>
2017-07-30 18:26:58 -0400

    updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -81,8 +81,8 @@ class Plugin {
 	public static function getMenu(GenericEvent $event) {
 		$menu = $event->getSubject();
 		if ($GLOBALS['tf']->ima == 'admin') {
-			$menu->add_link(self::$module, 'choice=none.reusable_fantastico', 'icons/database_warning_48.png', 'ReUsable Fantastico Licenses');
-			$menu->add_link(self::$module, 'choice=none.fantastico_list', 'icons/database_warning_48.png', 'Fantastico Licenses Breakdown');
+			$menu->add_link(self::$module, 'choice=none.reusable_fantastico', 'images/icons/database_warning_48.png', 'ReUsable Fantastico Licenses');
+			$menu->add_link(self::$module, 'choice=none.fantastico_list', 'images/icons/database_warning_48.png', 'Fantastico Licenses Breakdown');
 			$menu->add_link(self::$module.'api', 'choice=none.fantastico_licenses_list', 'whm/createacct.gif', 'List all Fantastico Licenses');
 		}
 	}
