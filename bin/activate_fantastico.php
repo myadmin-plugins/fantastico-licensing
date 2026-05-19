80ad6355dc3ca45fcff63a71e4de5400783b8dfd
Joe Huss <detain@interserver.net>
2026-05-19 12:44:45 -0400

    updates to cli scripts fixing ima sesison handling

--- bin/activate_fantastico.php
+++ bin/activate_fantastico.php
@@ -2,9 +2,9 @@
 <?php
 
 require_once __DIR__.'/../../../../include/functions.inc.php';
-\MyAdmin\App::session()->create(160308, 'services');
-\MyAdmin\App::session()->verify();
+\MyAdmin\App::session()->sessionid = substr(basename($_SERVER['argv'][0], '.php'), 0, 32);
+\MyAdmin\App::session()->account_id = 160308;
+\MyAdmin\App::session()->appnocache('ima', 'services');
+\MyAdmin\App::tf()->ima = 'services';
 
 activate_fantastico('66.23.229.238', 2);
-
-\MyAdmin\App::session()->destroy();
